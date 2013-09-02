988f82c2b3337ccf5306a9265b0b1788daf85995
Amund Tveit <amund@atbrox.com>
2013-09-02 10:53:48 +0200

    Initial Commit

---
+++ murmurhashforios/murmurhashforios/murmurhashforios.h
@@ -0,0 +1,13 @@
+//
+//  murmurhashforios.h
+//  murmurhashforios
+//
+//  Created by Amund Tveit on 9/2/13.
+//  Copyright (c) 2013 atbrox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface murmurhashforios : NSObject
+
+@end
---
+++ murmurhashforios/murmurhashforios/murmurhashforios.m
@@ -0,0 +1,13 @@
+//
+//  murmurhashforios.m
+//  murmurhashforios
+//
+//  Created by Amund Tveit on 9/2/13.
+//  Copyright (c) 2013 atbrox. All rights reserved.
+//
+
+#import "murmurhashforios.h"
+
+@implementation murmurhashforios
+
+@end
